fac27039933887ade979426d29625f4254d69918
ryp36466 <73660349+ryp36466@users.noreply.github.com>
2026-02-18 16:58:02 -0500

    Update raju.py

--- raju.py
+++ raju.py
@@ -331,7 +331,7 @@ with c3:
 st.divider()
 
 # Tabs
-tab1, tab2, tab3, tab4, tab5 = st.tabs([
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs([
     "🌎 Global Indices",
     "📈 Sectors, ETFs, 24h & Mag7",
     "📊 Relative Strength & Charts",
